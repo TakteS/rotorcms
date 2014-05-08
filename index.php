01c6256425be9c53cdf1a5d016fb8f93f1cde94b
visavi <visavi.net@mail.ru>
2014-05-08 13:22:30 +0400

    мелкие правки

--- index.php
+++ index.php
@@ -7,30 +7,25 @@
 #              ICQ  :  36-44-66               #
 #            Skype  :  vantuzilla             #
 #---------------------------------------------#
-
-
-
-
 require_once ('includes/start.php');
 require_once ('includes/functions.php');
 require_once ('includes/header.php');
 include_once ('themes/header.php');
 
+$data = explode('/', $request_uri);
 
+if (!empty($data[0])) {
 
-$data = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+	if ($data[0] == 'forum') {
+		$_GET['fid'] = $data[1];
+		include_once($data[0].'/forum.php');
+	}
 
-$_GET['fid'] = $data[1];
-include_once($data[0].'/forum.php');
+} else {
 
-
-
-
-//include_once (DATADIR.'/advert/top.dat');
-
-//render ('index');
-
-//include_once (DATADIR.'/advert/bottom.dat');
-
+	include_once (DATADIR.'/advert/top.dat');
+	render ('index');
+	include_once (DATADIR.'/advert/bottom.dat');
+}
 include_once ('themes/footer.php');
 ?>
